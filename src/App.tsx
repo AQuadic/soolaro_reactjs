58ca6f0599b38cab5d754a6a07718b36f1b2ac08
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-20 13:48:15 +0200

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/general/Layout";
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
