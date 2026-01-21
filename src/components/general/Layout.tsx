ac175bd86ad3df0c2f13a1209a8658ccad3d2292
moon <ahmedabozaid.dev@gmail.com>
2026-01-21 12:36:48 +0200

    removes layout animations

--- src/components/general/Layout.tsx
+++ src/components/general/Layout.tsx
@@ -1,34 +1,22 @@
-import { AnimatePresence, motion } from "framer-motion";
 import { useOutlet, useLocation } from "react-router";
 import { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = () => {
-    const outlet = useOutlet();
-    const location = useLocation();
+  const outlet = useOutlet();
+  const location = useLocation();
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: "instant" as ScrollBehavior });
-    }, [location.pathname]);
-    return (
-        <div>
-        <Header />
-        <AnimatePresence mode="wait">
-            <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -40 }}
-            transition={{ duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] }}
-            className="min-h-[80vh] "
-            >
-            {outlet}
-            </motion.div>
-        </AnimatePresence>
-        <Footer />
-        </div>
-    );
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" as ScrollBehavior });
+  }, [location.pathname]);
+  return (
+    <div>
+      <Header />
+      <div className="min-h-[80vh] ">{outlet}</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
