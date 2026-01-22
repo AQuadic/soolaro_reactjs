e3af1a89fd99d258f4bc1fce2840edbe6c0a2212
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-22 10:58:42 +0200

    adds complate selection section

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,3 +1,4 @@
+import ComplateSelection from "@/components/product_details/ComplateSelection"
 import ProductDetailsHeader from "@/components/product_details/ProductDetailsHeader"
 import ProductDetialsData from "@/components/product_details/ProductDetialsData"
 
@@ -6,6 +7,7 @@ const ProductDetailsPage = () => {
         <div>
             <ProductDetailsHeader />
             <ProductDetialsData />
+            <ComplateSelection />
         </div>
     )
 }
