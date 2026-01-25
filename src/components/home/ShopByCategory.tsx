af758718c7c977e4261020e808dd2444db674188
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-25 11:41:37 +0200

    translate shop by category section

--- src/components/home/ShopByCategory.tsx
+++ src/components/home/ShopByCategory.tsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 const ShopByCategory = () => {
+  const { t } = useTranslation("home");
   return (
     <section className="md:py-17 py-10 container">
       <motion.h2
@@ -10,7 +12,7 @@ const ShopByCategory = () => {
         viewport={{ once: true }}
         className="text-[#0B0B0B] md:text-[40px] text-lg font-semibold"
       >
-        Shop by category
+        {t("shop_by_category")}
       </motion.h2>
 
       <div className="md:mt-12 mt-6">
@@ -26,11 +28,11 @@ const ShopByCategory = () => {
               whileHover={{ scale: 1.05 }}
               transition={{ duration: 0.6 }}
               src="/images/home/best.jpg"
-              alt="best seller"
+              alt={t("best_seller")}
               className="w-full md:h-107 h-35.25 bg-[#0000001A] opacity-70 object-cover"
             />
             <h2 className="text-[#0B0B0B] md:text-[32px] text-sm font-semibold leading-[100%] absolute md:top-6 top-3 md:right-6 right-2">
-              Best seller
+              {t("best_seller")}
             </h2>
           </motion.div>
           <motion.div
@@ -44,11 +46,11 @@ const ShopByCategory = () => {
               whileHover={{ scale: 1.05 }}
               transition={{ duration: 0.6 }}
               src="/images/home/new.jpg"
-              alt="new"
+              alt={t("new_arrival")}
               className="w-full md:h-107 h-35.25 bg-[#0000001A] opacity-70 object-cover"
             />
             <h2 className="text-[#0B0B0B] md:text-[32px] text-sm font-semibold leading-[100%] absolute md:bottom-6 bottom-3 md:left-6 left-2">
-              New Arrival
+              {t("new_arrival")}
             </h2>
           </motion.div>
         </div>
@@ -63,11 +65,11 @@ const ShopByCategory = () => {
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.6 }}
             src="/images/home/summer.jpg"
-            alt="summer collection"
+            alt={t("summer_collection")}
             className="w-full md:h-107 h-39.5 bg-[#0000001A] opacity-70 object-cover"
           />
           <h2 className="text-[#0B0B0B] md:text-[32px] text-sm font-semibold leading-[100%] absolute md:top-6 top-3 md:left-6 left-2">
-            Summer collection
+            {t("summer_collection")}
           </h2>
         </motion.div>
       </div>
