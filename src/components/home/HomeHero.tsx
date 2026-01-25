a25890aa31c7be0f35903af9aeaa25f7a568c040
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-25 11:38:32 +0200

    home hero translation

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 import Arrow from "../icons/home/hero/Arrow";
 import HomeSlider from "./HomeSlider";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 const HomeHero = () => {
+  const { t } = useTranslation("home");
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -26,16 +28,13 @@ const HomeHero = () => {
           <motion.h2
             className="md:w-125 w-75 text-[#0B0B0B] md:text-[48px] text-2xl font-semibold leading-[150%]"
           >
-            Premium <span className="text-[#025D5B]">Eyewear </span> for Style
-            and <span className="text-[#025D5B]">Comfort!</span>
+            {t("premium")} <span className="text-[#025D5B]">{t("eyewear")}</span> {t("for_style")} <span className="text-[#025D5B]">{t("and_comfort")}</span>!
           </motion.h2>
 
           <motion.p
             className="md:w-146 text-[#3B3B3B] md:text-xl text-xs font-medium leading-[150%] mt-3"
           >
-            Explore a curated collection of glasses that combine modern design,
-            superior quality, and everyday comfort. Find the perfect pair that
-            suits your style and enhances your vision."
+            {t("hero_description")}
           </motion.p>
 
           <motion.button
@@ -45,7 +44,7 @@ const HomeHero = () => {
           >
             <Link to='/explore'>
               <p className="text-[#FEFEFE] md:text-lg text-base font-semibold">
-                Explore Shop
+                {t("explore_shop")}
               </p>
               <div className="absolute md:-top-2 -top-1 md:-right-2 -right-1">
                 <Arrow />
@@ -54,13 +53,13 @@ const HomeHero = () => {
           </motion.button>
 
           <motion.div
-            className="mt-4 md:flex hidden items-center gap-4"
+            className="mt-4 md:flex hidden items-center ltr:gap-4 rtl:gap-8"
           >
             <div className="flex items-center">
               <img
                 src="/images/home/customer.jpg"
                 alt="customer"
-                className="w-14 h-14 rounded-full border-2 border-white"
+                className="w-14 h-14 rounded-full border-2 border-white rtl:-ml-6"
               />
               <img
                 src="/images/home/customer.jpg"
@@ -84,7 +83,7 @@ const HomeHero = () => {
                 +50K
               </h3>
               <p className="text-[#3B3B3B] text-sm font-medium mt-2 leading-none">
-                Satisfied Customer
+                {t("satisfied_customer")}
               </p>
             </div>
           </motion.div>
