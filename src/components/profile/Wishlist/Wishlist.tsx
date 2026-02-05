79c15da8e825e7377cf2818cb0b53abf31a26598
moon <ahmedabozaid.dev@gmail.com>
2026-02-05 13:56:35 +0200

    fixes favorite

--- src/components/profile/Wishlist/Wishlist.tsx
+++ src/components/profile/Wishlist/Wishlist.tsx
@@ -3,7 +3,10 @@ import Card from "@/components/home/GlassCard";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "@tanstack/react-query";
-import { getFavorites, type FavoriteItem } from "@/lib/api/favorites/getFavorites";
+import {
+  getFavorites,
+  type FavoriteItem,
+} from "@/lib/api/favorites/getFavorites";
 import { Skeleton } from "@/components/ui/skeleton";
 import WhishlistEmptyState from "./WishlistEmptyState";
 
@@ -17,42 +20,41 @@ const Wishlist = () => {
 
   const skeletons = Array.from({ length: 6 });
 
-    return (
-        <section className="container">
-        <h1 className="text-[#0B0B0B] text-[40px] font-semibold mb-8 md:block hidden">
-            {t("myWishlist")}
-        </h1>
+  return (
+    <section className="container">
+      <h1 className="text-[#0B0B0B] text-[40px] font-semibold mb-8 md:block hidden">
+        {t("myWishlist")}
+      </h1>
 
-        <Link to="/" className="md:hidden flex items-center gap-3">
-            <MobileBackHeader />
-            <p className="text-[#0B0B0B] text-base font-semibold mb-6">
-            {t("favorite")}
-            </p>
-        </Link>
+      <Link to="/" className="md:hidden flex items-center gap-3">
+        <MobileBackHeader />
+        <p className="text-[#0B0B0B] text-base font-semibold mb-6">
+          {t("favorite")}
+        </p>
+      </Link>
 
-        {isLoading ? (
-            <div className="grid md:grid-cols-3 grid-cols-2 gap-8">
-            {skeletons.map((_, index) => (
-                <div key={index} className="h-[258px] w-full rounded-4xl overflow-hidden">
-                <Skeleton className="h-full w-full" />
-                </div>
-            ))}
+      {isLoading ? (
+        <div className="grid md:grid-cols-3 grid-cols-2 gap-8">
+          {skeletons.map((_, index) => (
+            <div
+              key={index}
+              className="h-[258px] w-full rounded-4xl overflow-hidden"
+            >
+              <Skeleton className="h-full w-full" />
             </div>
-        ) : favorites && favorites.length > 0 ? (
-            <div className="flex flex-wrap gap-8">
-            {favorites.map((item) => (
-                <Card
-                key={item.id}
-                showHeart
-                product={item as any}
-                />
-            ))}
-            </div>
-        ) : (
-            <WhishlistEmptyState />
-        )}
-        </section>
-    );
+          ))}
+        </div>
+      ) : favorites && favorites.length > 0 ? (
+        <div className="flex flex-wrap gap-8">
+          {favorites.map((item) => (
+            <Card key={item.id} showHeart product={item.favorable as any} />
+          ))}
+        </div>
+      ) : (
+        <WhishlistEmptyState />
+      )}
+    </section>
+  );
 };
 
 export default Wishlist;
