495a084e25c1700955b6134320f9cc051a15f278
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-03-11 08:58:06 +0200

    change arrow direction

--- src/components/icons/explore/BackArrow.tsx
+++ src/components/icons/explore/BackArrow.tsx
@@ -1,7 +1,7 @@
 const BackArrow = () => {
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15.0898 19.9201L8.56984 13.4001C7.79984 12.6301 7.79984 11.3701 8.56984 10.6001L15.0898 4.08008" stroke="#0B0B0B" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="transform rtl:scale-x-[-1]">
+            <path d="M15.0898 19.9201L8.56984 13.4001C7.79984 12.6301 7.79984 11.3701 8.56984 10.6001L15.0898 4.08008" stroke="#0B0B0B" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
     )
 }
